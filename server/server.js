5ab6a5fabd1e4b1a3081e4a615d2c3332f06de87
Ahmed Barbary <aelbarbary120@gmail.com>
2025-08-15 15:06:20 +0300

    updated

--- server/server.js
+++ server/server.js
@@ -64,6 +64,10 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello Greenmart!");
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
